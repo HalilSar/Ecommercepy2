d9cf5488291c8daa10776f556b31132a7a15a0eb
halil37 <hllibrahimsartitas@gmail.com>
2021-05-10 17:56:48 +0300

    Sorunlar giderildi

--- commerce/settings/production.py
+++ commerce/settings/production.py
@@ -4,4 +4,4 @@ with open(os.path.join(BASE_DIR,'secret_key.txt')) as f:
         SECRET_KEY = f.read().strip()
 DEBUG = False
 ALLOWED_HOSTS = ['halil37.pythonanywhere.com']
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
