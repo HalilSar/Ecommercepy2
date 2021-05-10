8269fc2fe37e6ce7e515b94c25b54fe72d8358de
halil37 <hllibrahimsartitas@gmail.com>
2021-05-10 14:13:58 +0300

    base.py problem giderildi.

--- commerce/settings/base.py
+++ commerce/settings/base.py
@@ -70,9 +70,11 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-             ],
+            ],
+        },
     },
 ]
+
 
 WSGI_APPLICATION = 'commerce.wsgi.application'
 
