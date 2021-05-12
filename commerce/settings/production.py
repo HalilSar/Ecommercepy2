f6a914c0dce84bcbe60f5173fd19697fe903d17e
halil37 <hllibrahimsartitas@gmail.com>
2021-05-12 16:08:03 +0300

    mysql process

--- commerce/settings/production.py
+++ commerce/settings/production.py
@@ -4,13 +4,27 @@ with open(os.path.join(BASE_DIR,'secret_key.txt')) as f:
         SECRET_KEY = f.read().strip()
 DEBUG = False
 ALLOWED_HOSTS = ['halil37.pythonanywhere.com']
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'Halil37$default',
+#         'USER': 'Halil37',
+#         'PASSWORD': 'covboybebop123',
+#         'HOST':'Halil37.mysql.pythonanywhere-services.com',
+#     }
+#}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': '/path/to/my.cnf',
+#         },
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Halil37$default',
-        'USER': 'Halil37',
-        'PASSWORD': 'covboybebop123',
-        'HOST':'Halil37.mysql.pythonanywhere-services.com',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
